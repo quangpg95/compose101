044263f57acd9792e4c08bc2be50621c18677f0d
quang.nd1 <quang.nd1@kiotviet.com>
2023-12-19 09:52:42 +0700

    done init

--- app/src/main/java/com/qq/compose101/MainActivity.kt
+++ app/src/main/java/com/qq/compose101/MainActivity.kt
@@ -3,14 +3,23 @@ package com.qq.compose101
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,27 +41,55 @@ private fun ComposeApp101(
     modifier: Modifier = Modifier,
     names: List<String> = listOf("World", "Compose")
 ) {
-    // A surface container using the 'background' color from the theme
     Surface(
         color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Column(modifier = Modifier.padding(vertical = 8.dp, horizontal = 8.dp)) {
-            for (name in names) {
-                Greeting(name = name)
-            }
+//        Column(modifier = modifier.padding(vertical = 4.dp)) {
+//            for (name in names) {
+//                Greeting(name = name)
+//            }
+//        }
+        Greetings()
+    }
+}
+
+@Composable
+fun Greetings(modifier: Modifier = Modifier, names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = modifier.padding(4.dp)) {
+        items(names) { name ->
+            Greeting(name)
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Column {
-        Text(
-            text = "Hello",
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
         )
-        Text(text = "$name!")
+    )
+    Row(Modifier.padding(24.dp)) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)
+        ) {
+            Text(
+                text = "Hello",
+            )
+            Text(text = "$name!")
+        }
+        ElevatedButton(onClick = { expanded = !expanded }) {
+            Text(if (expanded) "Show less" else "Show more")
+        }
     }
+
+
 }
 
 @Preview(showBackground = true, widthDp = 320)
