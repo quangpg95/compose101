f5458fc2dd9c9f6ed336c7aa1e3d2e7e25c6cb50
Quang Nguyen <quangpg95@macoss-Mac-mini.local>
2023-12-21 22:25:27 +0700

    config route

---
+++ app/src/main/java/com/qq/compose101/screen/Compose101App.kt
@@ -0,0 +1,18 @@
+package com.qq.compose101.screen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+
+@Composable
+fun Compose101App() {
+
+}
+
+@Composable
+fun Compose101NavHost(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = Screen.Home.route) {
+
+    }
+
+}
