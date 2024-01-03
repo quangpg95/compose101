3343210c2b41a7a2a079cbceb0e1ea5d26cefbc0
quangpg95 <quangpg95@gmail.com>
2024-01-03 18:33:09 +0700

    refactor viewmodel

--- app/src/main/java/com/qq/compose101/feature/plants/ui/viewModel/GardenModel.kt
+++ app/src/main/java/com/qq/compose101/feature/plants/ui/viewModel/GardenModel.kt
@@ -1,28 +1,34 @@
 package com.qq.compose101.feature.plants.ui.viewModel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.qq.compose101.core.platform.BaseViewModel
 import com.qq.compose101.core.usecase.UseCase
 import com.qq.compose101.feature.plants.domain.entity.PlantAndGardenPlantings
 import com.qq.compose101.feature.plants.domain.usecase.GetGardens
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class GardenModel @Inject constructor(getGardens: GetGardens) : BaseViewModel() {
-    val _plantAndGardenPlantings = MutableLiveData<List<PlantAndGardenPlantings>>()
-    val plantAndGardenPlantings: LiveData<List<PlantAndGardenPlantings>> get() = _plantAndGardenPlantings
+    private val _gardenPlantingState = MutableStateFlow<List<PlantAndGardenPlantings>>(emptyList())
+    val gardenPlantingState: StateFlow<List<PlantAndGardenPlantings>> get() = _gardenPlantingState
 
-//    init {
-//        getGardens.invoke(UseCase.None()) {
-//            it.fold(::handleFailure, ::handleGardenList)
-//        }
-//    }
-//
-//    fun handleGardenList(plantAndGardenPlantings: Flow<List<PlantAndGardenPlantings>>) {
-//        _plantAndGardenPlantings.postValue(plantAndGardenPlantings.asLiveData())
-//    }
+    init {
+        getGardens.invoke(UseCase.None()) {
+            it.fold(::handleFailure, ::handleGardenList)
+        }
+    }
+
+    private fun handleGardenList(plantAndGardenPlantings: Flow<List<PlantAndGardenPlantings>>) {
+        viewModelScope.launch {
+            plantAndGardenPlantings.collectLatest {
+                _gardenPlantingState.value = it
+            }
+        }
+    }
 }
--- app/src/main/java/com/qq/compose101/feature/plants/ui/viewModel/PlantListViewModel.kt
+++ app/src/main/java/com/qq/compose101/feature/plants/ui/viewModel/PlantListViewModel.kt
@@ -1,12 +1,38 @@
 package com.qq.compose101.feature.plants.ui.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.qq.compose101.core.platform.BaseViewModel
+import com.qq.compose101.core.usecase.UseCase
+import com.qq.compose101.feature.plants.domain.entity.Plant
+import com.qq.compose101.feature.plants.domain.entity.PlantAndGardenPlantings
 import com.qq.compose101.feature.plants.domain.usecase.GetPlants
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class PlantListViewModel @Inject constructor(private val getPlants: GetPlants) : ViewModel() {
+class PlantListViewModel @Inject constructor(private val getPlants: GetPlants) : BaseViewModel() {
+    private val _plants = MutableStateFlow<List<Plant>>(emptyList())
+    val plants: StateFlow<List<Plant>> get() = _plants
+
+    init {
+        getPlants.invoke(UseCase.None()) {
+            it.fold(::handleFailure, ::handlePlantList)
+        }
+    }
+
+    private fun handlePlantList(plants: Flow<List<Plant>>) {
+        viewModelScope.launch {
+            plants.collectLatest {
+                _plants.value = it
+            }
+        }
+    }
 
 
 }
